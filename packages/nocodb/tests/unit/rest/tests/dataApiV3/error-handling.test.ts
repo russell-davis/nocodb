2a9ab03932c1b2766ca28c5cd42822e76e05f5d0
Fendy Heryanto <fendy@nocodb.com>
2025-05-07 04:02:08 +0000

    fix unit test errors

--- packages/nocodb/tests/unit/rest/tests/dataApiV3/error-handling.test.ts
+++ packages/nocodb/tests/unit/rest/tests/dataApiV3/error-handling.test.ts
@@ -341,27 +341,31 @@ describe('dataApiV3', () => {
       });
 
       it('primary key not in correct data type (Id)', async () => {
-        const table = await createTable(testContext.context, testContext.base, {
-          table_name: 'testTable',
-          title: 'TestTable',
-          columns: [
-            {
-              column_name: 'id',
-              title: 'Id',
-              uidt: UITypes.ID,
-              pk: true,
-            },
-            {
-              column_name: 'text',
-              title: 'Text',
-              uidt: UITypes.SingleLineText,
-              pk: false,
-              pv: true,
-            },
-          ],
-        });
+        const table = await createTable(
+          testContext.context,
+          testContext.sakilaProject,
+          {
+            table_name: 'testTable',
+            title: 'TestTable',
+            columns: [
+              {
+                column_name: 'id',
+                title: 'Id',
+                uidt: UITypes.ID,
+                pk: true,
+              },
+              {
+                column_name: 'text',
+                title: 'Text',
+                uidt: UITypes.SingleLineText,
+                pk: false,
+                pv: true,
+              },
+            ],
+          },
+        );
         await createBulkRows(testContext.context, {
-          base: testContext.base,
+          base: testContext.sakilaProject,
           table,
           values: [
             {
@@ -535,7 +539,7 @@ describe('dataApiV3', () => {
         const values = ['HELLOW', -1, 9999];
         for (const value of values) {
           const response = await ncAxiosPost({
-            url: `${urlPrefix}/${table.id}`,
+            url: `${numberBasedUrlPrefix}/${table.id}`,
             body: {
               Rating: value,
             },
@@ -554,7 +558,7 @@ describe('dataApiV3', () => {
           return;
         }
         const response = await ncAxiosPost({
-          url: `${urlPrefix}/${table.id}`,
+          url: `${numberBasedUrlPrefix}/${table.id}`,
           body: [
             {
               Rating: 99,
@@ -573,7 +577,7 @@ describe('dataApiV3', () => {
         }
         for (const year of [99, 19999, 'HELLOW', 19.778, -123]) {
           const response = await ncAxiosPost({
-            url: `${urlPrefix}/${table.id}`,
+            url: `${numberBasedUrlPrefix}/${table.id}`,
             body: [
               {
                 Year: year,
@@ -594,7 +598,7 @@ describe('dataApiV3', () => {
         const values = ['HELLOW', -1, 1.365];
         for (const value of values) {
           const response = await ncAxiosPost({
-            url: `${urlPrefix}/${table.id}`,
+            url: `${numberBasedUrlPrefix}/${table.id}`,
             body: {
               Duration: value,
             },
@@ -828,10 +832,12 @@ describe('dataApiV3', () => {
     describe('user-based', () => {
       let table: Model;
       let columns: Column[] = [];
+      let userBasedUrlPrefix: string = '';
 
       beforeEach(async () => {
         const initResult = await beforeEachUserBased(testContext);
 
+        userBasedUrlPrefix = `/api/${API_VERSION}/${testContext.base.id}`;
         table = initResult.table;
         columns = initResult.columns;
       });
@@ -844,7 +850,7 @@ describe('dataApiV3', () => {
           userFieldMulti: `${userList[0].id},${userList[0].id}`,
         };
         const rsp = await ncAxiosPost({
-          url: `${urlPrefix}/${table.id}`,
+          url: `${userBasedUrlPrefix}/${table.id}`,
           body: newRecord1,
           status: 422,
         });
@@ -859,7 +865,7 @@ describe('dataApiV3', () => {
           userFieldMulti: `${userList[0].id},${userList[1].id}`,
         };
         const rsp2 = await ncAxiosPost({
-          url: `${urlPrefix}/${table.id}`,
+          url: `${userBasedUrlPrefix}/${table.id}`,
           body: newRecord2,
           status: 422,
         });
