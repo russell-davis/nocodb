0b06d23a210025a057e799a1ed4741e4044acb52
Fendy Heryanto <fendy@nocodb.com>
2025-05-08 08:18:48 +0000

    optional hookName

--- packages/nocodb/src/helpers/webhookHelpers.ts
+++ packages/nocodb/src/helpers/webhookHelpers.ts
@@ -822,7 +822,7 @@ export async function invokeWebhook(
           ) {
             hookLog = {
               ...hook,
-              operation: hookName.split('.')?.[1] as any,
+              operation: hookName?.split('.')?.[1] as any,
               fk_hook_id: hook.id,
               type: notification.type,
               payload: JSON.stringify(notification?.payload),
@@ -862,7 +862,7 @@ export async function invokeWebhook(
     ) {
       hookLog = {
         ...hook,
-        operation: hookName.split('.')?.[1] as any,
+        operation: hookName?.split('.')?.[1] as any,
         type: notification.type,
         payload: JSON.stringify(
           reqPayload
